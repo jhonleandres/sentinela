14c7cb918aab38691335667debf15be9ff11114d
JHONLEANDRES <jhonleandres.silva@gmail.com>
2020-11-04 23:20:47 -0300

    configuration acess db tenant

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TenantModule } from './tenant/tenant.module';
+import { ProductController } from './product/product.controller';
+import { ProductModule } from './product/product.module';
+import * as ormconfig from '../ormconfig';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot(ormconfig),
+    TenantModule,
+    ProductModule,
+  ],
+  controllers: [AppController, ProductController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:4200', // angular
+  //     'http://localhost:3000', // react
+  //     'http://localhost:8081', // react-native
+  //   ],
+  // });
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
